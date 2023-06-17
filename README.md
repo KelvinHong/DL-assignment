2c6263489cd1deb933cb87cd4c481976780e82ce
Kelvin Hong <kh.boon2@gmail.com>
2023-06-17 20:50:41 +0800

    Done preparing dataset

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ such model can visualize regions of input image that activates the
 decision of the model, which is typically used to interpret 
 the inner working of an image classifier. 
 
+## Pre-requisite
+
+This program is tested on Python 3.9.0, but should work on later version
+of python. 
+
 ## Steps to use this repository. 
+
+Run `python prepare.py` to download dataset 
--- prepare.py
+++ prepare.py
@@ -1,17 +1,39 @@
 import requests
+import os
+import tarfile
+
+data_dir = "./data/"
+os.makedirs(data_dir, exist_ok=True)
+
+# Download split 
+split_url = "https://www.robots.ox.ac.uk/~vgg/data/flowers/17/datasplits.mat"
+split_path = os.path.join(data_dir, "datasplits.mat")
+response = requests.get(split_url, stream=True)
+if response.status_code == 200:
+    with open(split_path, 'wb') as f:
+        f.write(response.raw.read())
+    print("Split file downloaded. ")
 
 print("Downloading dataset tar file, please wait for a few seconds...")
 url = 'https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz'
-target_path = './data/17flowers.tgz'
+target_path = os.path.join(data_dir,'17flowers.tgz')
 
+# Download tar file
 response = requests.get(url, stream=True)
 if response.status_code == 200:
     with open(target_path, 'wb') as f:
         f.write(response.raw.read())
-    print("File downloaded. ")
+    print("Dataset file downloaded. ")
 else:
     print("File couldn't be downloaded, please download it " +\
         "from https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz" +\
         "manually and put it in the \"data/\" folder")
+    exit()
 
+# Decompress tar file.
+tar = tarfile.open(target_path, "r")
+tar.extractall(path=data_dir)
+tar.close()
 
+# Remove tgz file
+os.remove(target_path)
